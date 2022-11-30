a8361ddb67a59223c29b88a8f09b00c744d4f69e
thivagark-apps <116530154+thivagark-apps@users.noreply.github.com>
2022-11-30 13:33:42 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.109.133.39:9000/',
+  serverUrl: 'http://3.110.31.125:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
